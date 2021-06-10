eea7e1e5a0f237764072e917eace27e6252bca42
JeUpark <genhcode@gmail.com>
2021-06-10 10:46:46 +0900

    #3.3 GlobalStyles and Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,17 +3,21 @@ import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom"
 import Home from "../Routes/Home"
 import TV from "../Routes/TV"
 import Search from "../Routes/Search"
+import Header from "../Components/Header"
 
 
 const Router = () => ( 
-    <BrowserRouter>
+  <BrowserRouter>
+    <>
+      <Header/>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
         <Route path="/search" component={Search} />
         <Redirect from="*" to="/" />
       </Switch>
-    </BrowserRouter>
-  )
+    </>
+  </BrowserRouter>
+)
 
 export default Router;

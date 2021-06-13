688c3eb803fc6f59ae645b7732d45ed0a22aae8c
JeUpark <genhcode99@gmail.com>
2021-06-14 07:58:47 +0900

    #6.5 Poster Component part One

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
+
 
 
 const Container = styled.div`
@@ -16,19 +18,25 @@ const HomePresenter = ({nowPlaying, upcoming, popular, error, loading}) => loadi
     {
       nowPlaying && nowPlaying.length > 0 && (
       <Section title="Now Playing">
-        {nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        {nowPlaying.map(movie => 
+          <Poster/>
+        )}
       </Section>
     )}
     {
       upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movies">
-          {upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}
+          {upcoming.map(movie => 
+            <Poster/>
+          )}
         </Section>
     )}
     {
     popular && popular.length > 0 && (
       <Section title="Popular Movies">
-        {popular.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        {popular.map(movie => 
+          <Poster/>
+        )}
       </Section>
     )}
     { error && <Message text={error} color="e74c3c"/> }

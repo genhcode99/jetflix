9b622df6df53eab3c875191df119b18960f84970
JeUpark <genhcode99@gmail.com>
2021-06-16 08:03:26 +0900

    test : for youtube 1

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -18,7 +18,6 @@ const Router = () => (
         <Route path="/search" component={Search} />
         <Route path="/movie/:id" component={Detail} />
         <Route path="/show/:id" component={Detail} />
-
         <Redirect from="*" to="/" />
       </Switch>
     </>
